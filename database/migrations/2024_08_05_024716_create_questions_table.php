c60e73316c72f802275594c645969ab2caa884b8
MetzkerDeveloper <walerianoadm@gmail.com>
2024-08-05 00:05:53 -0300

    HEY-9: HEY-9: it should be able to create a new question bigger than 255 characters

--- database/migrations/2024_08_05_024716_create_questions_table.php
+++ database/migrations/2024_08_05_024716_create_questions_table.php
@@ -12,6 +12,7 @@ return new class () extends Migration {
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->text('question');
             $table->timestamps();
         });
     }
