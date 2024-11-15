cb4b04f6813b31bcdf3c4e94347ed4ddb34d09e0
MetzkerDeveloper <walerianoadm@gmail.com>
2024-11-15 18:16:47 -0300

    hey-12: being able to see the list of questions in the dashboard.blade.php

---
+++ tests/Feature/ListQuestionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, get};
+
+it('should list all the questions', closure: function () {
+    // Arrange
+    // Criar algumas perguntas
+    $user      = User::factory()->create();
+    $questions = Question::factory()->count(5)->create();
+
+    actingAs($user);
+    // Act
+    // Acessar a rota
+    $response = get(route('dashboard'));
+
+    // Assert
+    // Verificar se a lista de perguntas está sendo mostrada
+
+    /** @var Question $q */
+    foreach ($questions as $q) {
+
+        $response->assertSee($q->question);
+
+    }
+
+});
