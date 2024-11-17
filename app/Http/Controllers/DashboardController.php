52de950cf874cb6d7f423a724c01bf14d8034d94
MetzkerDeveloper <walerianoadm@gmail.com>
2024-11-16 23:42:28 -0300

    hey-12: handling N+1 issue

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,9 @@ class DashboardController extends Controller
 {
     public function __invoke(): Factory|View
     {
-        $questions = Question::all();
+        $questions = Question::withSum('votes', 'like')
+            ->withSum('votes', 'unlike')
+            ->get();
 
         return view('dashboard', compact('questions'));
     }
